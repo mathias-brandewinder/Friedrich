6a4bd9215ece36245befdf900db5c37dd99c5399
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-11-06 15:49:03 -0800

    Basic regression with gradient descent

---
+++ Friedrich/Friedrich.Tests/Regression.fs
@@ -0,0 +1,68 @@
+﻿namespace Friedrich.Tests
+
+open MathNet
+open Friedrich.Regression
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``Regression unit tests`` () = 
+    
+    [<Test>]
+    member this.``Simple Regression, intercept`` () =
+
+        // Y = 1 + 2 * X1 - 3 * X2
+
+        let X = 
+            [|  [| 1.; 1. |];
+                [| 1.; 0. |];
+                [| 0.; 1. |];
+            |]
+
+        let Y = [| 0.; 3.; -2. |]
+        let W = [| 1.; 1.; 1.; |]
+
+        let result = estimate (Y,X,W) true 0.5 0. 100
+        
+        result.[0] |> should (equalWithin 0.1) 1.
+        result.[1] |> should (equalWithin 0.1) 2.
+        result.[2] |> should (equalWithin 0.1) -3.
+
+    [<Test>]
+    member this.``Simple Regression, no intercept`` () =
+
+        // Y = 0 + 2 * X1
+
+        let X = 
+            [|  [| 1.; |];
+                [| 1.; |];
+            |]
+
+        let Y = [| 1.; 3.; |] // off the diagonal by +1, -1
+        let W = [| 1.; 1.; |]
+
+        let result = estimate (Y,X,W) false 0.5 0. 10
+        
+        result.[0] |> should (equalWithin 0.1) 0.
+        result.[1] |> should (equalWithin 0.1) 2.
+
+    [<Test>]
+    member this.``Regularized Regression, intercept`` () =
+
+        // Y = 1 + 2 * X1 - 3 * X2
+
+        let X = 
+            [|  [| 1.; 1. |];
+                [| 1.; 0. |];
+                [| 0.; 1. |];
+            |]
+
+        let Y = [| 0.; 3.; -2. |]
+        let W = [| 1.; 1.; 1.; |]
+
+        let result = estimate (Y,X,W) true 0.5 0.1 100
+        
+        // Computed using another solver
+        result.[0] |> should (equalWithin 0.1) 0.84
+        result.[1] |> should (equalWithin 0.1) 1.89
+        result.[2] |> should (equalWithin 0.1) -2.66
---
+++ Friedrich/Friedrich/Regression.fs
@@ -0,0 +1,72 @@
+﻿namespace Friedrich
+
+module Regression =
+
+    open System
+    open MathNet.Numerics.LinearAlgebra.Double
+
+    // 0 out first element of Theta for regularization.
+    let private reducedTheta (theta: Vector) = 
+        theta 
+        |> Vector.mapi (fun i x -> if i = 0 then 0. else x)
+
+    // cost function: sum of squares of distance
+    // between observed and predicted, and 
+    // regularization term (norm of Theta)
+    let cost (x: DenseMatrix) (y: DenseVector) (w: DenseVector) (theta: DenseVector) (lambda: float) = 
+        let m = y.Count |> (float)
+        let theta' = reducedTheta theta
+        ((y - x * theta).PointwiseMultiply(w) * (y - x * theta)) / (1. * m) 
+        + (lambda / (2. * m)) * (theta' * theta')
+
+    let private descentUpdate (x: DenseMatrix) (y: DenseVector) (w: DenseVector) (theta: DenseVector) (alpha: float) (lambda: float) =
+        let m = y.Count |> (float)
+        let gradient = 
+            ((1. / m) * (y - (x * theta)).PointwiseMultiply(w) * x
+            - (1. / m) * lambda * (reducedTheta theta)) :?> DenseVector
+        (theta + alpha * gradient) 
+
+    let private run (x: DenseMatrix) (y: DenseVector) (w: DenseVector) (intercept:bool) (alpha: float) (lambda: float) (iters: int) =
+        if not (x.RowCount = y.Count) 
+        then failwith "Inconsistent sizes"
+        else
+            let features = x.ColumnCount
+            let x' =
+                if intercept 
+                then x.InsertColumn(0, DenseVector.create y.Count 1.) :?> DenseMatrix
+                else  x.InsertColumn(0, DenseVector.create y.Count 0.) :?> DenseMatrix
+            let theta = DenseVector(features + 1)
+
+            let rec updater i t =
+
+                let t' = descentUpdate x' y w t alpha lambda
+                let curr = cost x' y w t' lambda
+                System.Console.WriteLine(curr)
+                if i > iters then t'
+                else updater (i + 1) t'
+
+            updater 0 theta 
+
+    let linear (x: DenseVector) (th: DenseVector) = (Vector.insert 0 1. x * th)
+  
+    let estimate (data: float [] * float [][] * float []) // output, inputs, obs weights
+                 intercept alpha lambda iters =
+
+        let Ys, Xs, Ws = data
+
+        let features = Xs.[0].Length
+        let sample = Ys.Length
+    
+        // convert to algebra form
+        let y = DenseVector(sample)
+        let x = DenseMatrix(sample, features)
+        let w = DenseVector(sample)
+    
+        // fill in matrices & vectors
+        for row in 0 .. (sample - 1) do
+            for col in 0 .. (features - 1) do
+                x.[row,col] <- Xs.[row].[col]
+            y.[row] <- Ys.[row]
+            w.[row] <- Ws.[row]
+
+        run x y w intercept alpha lambda iters    
